2410ce4296ba6c20daa37f36a7fdc8474462947f
autowp <dmitry@pereslegin.ru>
2013-03-19 21:09:27 +0400

    some

---
+++ application/languages/ru.php
@@ -0,0 +1,69 @@
+<?php
+
+return array (
+    'few seconds ago'   => 'несколько секунд назад',
+    'a minute ago'      => 'минуту назад',
+    '%1$s minutes ago/1'    => '%1$s минуту назад',
+    '%1$s minutes ago/2'    => '%1$s минуты назад',
+    '%1$s minutes ago/5'    => '%1$s минут назад',
+    'an hour ago'       => 'час назад',
+    '%1$s hours ago/1'  => '%1$s час назад',
+    '%1$s hours ago/2'  => '%1$s часа назад',
+    '%1$s hours ago/5'  => '%1$s часов назад',
+    'yesterday'         => 'вчера',
+    'today'             => 'сегодня',
+
+    /* Picture preview */
+    'picture-preview/no-comments'   =>  'нет',
+	'picture-preview/no-ratio'		=>	'нет',
+    'Resolution'    => 'Разрешение',
+    'Filesize'      => 'Размер',
+    'Ratio'         => 'Оценка',
+    'Views'         => 'Просмотров',
+    'Comments count' => 'Комментариев',
+
+    /* SPECIFICATIONS*/
+    'rpm'   => 'об/мин',
+    'Nm'    => 'Нм',
+    'kW'    => 'КВт',
+    'hp'    => 'л.с.',
+
+    /* PAGINATOR */
+    'next'      => 'следующая',
+    'previous'  => 'предыдущая',
+
+    /* INDEX */
+    'back'      => 'назад',
+    'forward'   => 'вперед',
+
+    'Picture of the day'    => 'Картинка дня',
+	'car-of-day'   			=> 'автомобиль дня',
+    'day ahead'             => 'день вперед',
+    'day ago'               => 'день назад',
+    'Cars by destination'   => 'Автомобили по назначению',
+    'All new pictures'      => 'все новые картинки',
+	
+	'all-link'	=>	'все',
+	
+	'brands/new-cars-of-brand'	=>	'Новые автомобили %s',
+
+    /* Layout */
+    'breadcrumbs/title' =>  'Путь - дорога',
+
+    /* BRAND PAGE*/
+    'unsorted'                  =>  'Несортировано',
+    'engines'                   =>  'Двигатели',
+    'concepts and prototypes'   =>  'Концепты и прототипы',
+    'logotypes'                 =>  'Логотипы',
+    'mixed'                     =>  'Разное',
+
+    'count 0'   =>  'нет',
+
+    /* CAR LIST */
+    'carlist/all pictures'      =>  'все картинки',
+    'carlist/twins'             =>  'автомобили-близнецы',
+	'carlist/produced/one'		=>	'Произведен в единственном экземпляре',
+	'carlist/specifications'	=>	'технические характеристики',
+
+	'present-time-abbr'			=>	'н.в.'
+);
