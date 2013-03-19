3a1d5865391d8b0221436301700027693e2e03d4
autowp <dmitry@pereslegin.ru>
2013-03-19 11:18:07 +0400

    cleanup

--- public_html/js/banner/mixmarket.h.js
+++
@@ -1,1 +0,0 @@
-document.write('<scr' + 'ipt language="javascript" type="text/javascript" src="http://mixmarket.biz/show/4294948675/&div=mixkt_4294948675&r=' + escape(document.referrer) + '&rnd=' + Math.round(Math.random() * 100000) + '" charset="utf-8"><' + '/scr' + 'ipt>');
