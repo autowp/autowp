c2ebdf20734e909272a8718c424701199d765590
autowp <dmitry@pereslegin.ru>
2013-04-03 23:53:28 +0400

    model name form fix

--- library/Project/Form/Element/Model/Name.php
+++ library/Project/Form/Element/Model/Name.php
@@ -4,18 +4,21 @@ class Project_Form_Element_Model_Name extends Zend_Form_Element_Text
 {
     public function init()
     {
+        parent::init();
+
         $this->setOptions(array(
             'maxlength'  => 80,
             'size'       => 60,
             'label'      => 'Название',
-            'filters'    => array(
-                'StringTrim', 'SingleSpaces'
-            ),
-            'validators' => array(
-                array('StringLength', true, array (1, 80))
-            )
         ));
 
-        parent::init();
+        $this->addFilters(array(
+            'StringTrim',
+            'SingleSpaces'
+        ));
+
+        $this->addValidators(array(
+            array('StringLength', true, array (1, 80))
+        ));
     }
 }
