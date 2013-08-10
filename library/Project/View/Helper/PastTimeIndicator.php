bc2a59a4d1d3812982e4e7885a951820427517c5
autowp <dmitry@pereslegin.ru>
2013-08-10 20:04:30 +0400

    PastTimeIndicator icon fix

--- library/Project/View/Helper/PastTimeIndicator.php
+++ library/Project/View/Helper/PastTimeIndicator.php
@@ -2,20 +2,20 @@
 
 class Project_View_Helper_PastTimeIndicator extends Zend_View_Helper_Abstract
 {
-	/**
-	 * @var Zend_Date
-	 */
-	protected $_pastLimit;
-	
-	public function __construct()
-	{
-		$this->_pastLimit = Zend_Date::now()->subDay(1);
-	}
-	
-	public function pastTimeIndicator(Zend_Date $date)
-	{
-		$icon = $date->isLater($this->_pastLimit) ? 'icon-time' : 'icon-calendar';
-		
-		return '<i class="' . $icon . '"></i> ' . $this->view->escape($this->view->humanTime($date));
-	}
+    /**
+     * @var Zend_Date
+     */
+    protected $_pastLimit;
+
+    public function __construct()
+    {
+        $this->_pastLimit = Zend_Date::now()->subDay(1);
+    }
+
+    public function pastTimeIndicator(Zend_Date $date)
+    {
+        $icon = $date->isLater($this->_pastLimit) ? 'glyphicon-time' : 'glyphicon-calendar';
+
+        return '<i class="glyphicon ' . $icon . '"></i> ' . $this->view->escape($this->view->humanTime($date));
+    }
 }
