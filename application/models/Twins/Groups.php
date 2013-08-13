d3904cd0747969ad86f95658796d363fd6178163
autowp <dmitry@pereslegin.ru>
2013-08-14 00:35:49 +0400

    Twins tables classes

---
+++ application/models/Twins/Groups.php
@@ -0,0 +1,7 @@
+<?php
+
+class Twins_Groups extends Zend_Db_Table
+{
+    protected $_name = 'twins_groups';
+    protected $_rowClass = 'Twins_Groups_Row';
+}
---
+++ application/models/Twins/Groups/Row.php
@@ -0,0 +1,5 @@
+<?php
+
+class Twins_Groups_Row extends My_Db_Table_Row
+{
+}
