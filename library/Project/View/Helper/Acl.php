4bf58366c914ae63cf5ccddf3de71eb9178386fd
autowp <dmitry@pereslegin.ru>
2013-08-20 00:33:07 +0400

    Acl helper whitespaces

--- library/Project/View/Helper/Acl.php
+++ library/Project/View/Helper/Acl.php
@@ -2,12 +2,12 @@
 
 class Project_View_Helper_Acl extends Zend_View_Helper_Abstract
 {
-	/**
-	 * @return Project_Acl
-	 */
-	public function acl()
-	{
-		return Zend_Controller_Front::getInstance()
-			->getParam('bootstrap')->getResource('acl');
-	}
+    /**
+     * @return Project_Acl
+     */
+    public function acl()
+    {
+        return Zend_Controller_Front::getInstance()
+            ->getParam('bootstrap')->getResource('acl');
+    }
 }
