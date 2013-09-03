e98e3b7deca35373bb7c87f20d63e88f0ec00581
autowp <dmitry@pereslegin.ru>
2013-09-04 02:07:42 +0400

    Move brand logos to image storage, sprite generator 4 future use

--- library/Project/Validate/Brand/NameNotExists.php
+++ library/Project/Validate/Brand/NameNotExists.php
@@ -9,7 +9,7 @@ class Project_Validate_Brand_NameNotExists extends Zend_Validate_Abstract
         $this->_messages = array();
 
         $brands = new Brands();
-        $row = $brands->findByCaption($value)->current();
+        $row = $brands->fetchRowByCaption($value);
         if ($row) {
             $this->_messages[self::EXISTS] = sprintf("Бренд с названием '%s' уже существует", $row->caption);
             return false;
