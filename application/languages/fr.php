e1618e07bc0279fe206955ab58f9ec9b01b3703e
Dmitry <dmitry@pereslegin.ru>
2016-01-08 13:28:11 +0100

    Francais lang file

---
+++ application/languages/fr.php
@@ -0,0 +1,194 @@
+<?php
+
+return array (
+    /* Common words */
+    'and' => 'et',
+    '%1$s picures'       => array(
+        '%1$s photo',
+        '%1$s photos',
+    ),
+    '%1$s vehicles'      => array(
+        '%1$s véhicule',
+        '%1$s véhicules',
+    ),
+    '%1$s comments'      => array(
+        '%1$s commentaire',
+        '%1$s commentaires',
+    ),
+
+    'few seconds ago'    => 'few seconds ago',
+    'a minute ago'       => 'a minute ago',
+    '%1$s minutes ago'   => array(
+        '%1$s minute ago',
+        '%1$s minutes ago'
+    ),
+    'an hour ago'        => 'an hour ago',
+    '%1$s hours ago'     => array(
+        '%1$s hour ago',
+        '%1$s hours ago'
+    ),
+    'yesterday'          => 'hier',
+    'today'              => 'aujourd\'hui',
+
+    'contributor' => 'contributeur',
+    'contributors' => 'contributeurs',
+
+    /* Layout */
+    'layout/brand' => 'WheelsAge.org',
+    'layout/personal-messages' => 'Messages personnels',
+    'layout/you-have-%1$s-new-messages' => array(
+        'Vous avez %1$s nouveau message',
+        'Vous avez %1$s nouveaux messages',
+    ),
+
+    'layout/search' => 'Recherche',
+
+    /* Picture preview */
+    'picture-preview/no-comments' => 'non',
+    'picture-preview/no-ratio'    => 'non',
+    'Resolution'                  => 'Résolution',
+    'Filesize'                    => 'Taille du fichier',
+    'Ratio'                       => 'Ratio',
+    'Views'                       => 'Vues',
+    'Comments count'              => 'Commentaires',
+
+    /* SPECIFICATIONS*/
+    'rpm' => 'rpm',
+    'Nm'  => 'Nm',
+    'kW'  => 'kW',
+    'hp'  => 'hp',
+    'spec/%1$s-gear' => array(
+        '%1$s vitesse',
+        '%1$s vitesses'
+    ),
+
+    /* PAGINATOR */
+    'next'                     => 'prochaine',
+    'previous'                 => 'précédent',
+
+    /* INDEX */
+    'index/brands'             => 'Marques',
+
+    'back'                     => 'arrière',
+    'forward'                  => 'vers l\'avant',
+
+    'Picture of the day'       => 'Image du jour',
+    'car-of-day'               => 'voiture de la journée',
+    'day ahead'                => 'journée à l\'avance',
+    'day ago'                  => 'il ya jour',
+    'Cars by destination'      => 'Cars by destination',
+    'All new pictures'         => 'toutes les nouvelles images',
+
+    'all-link'                 => 'tous',
+
+    'brands/new-cars-of-brand' => 'Nouveaux modèles %s',
+
+    'brands/more-companies'    => array(
+        'plus d\'%s marque',
+        'plus de %s marques'
+    ),
+
+    /* Layout */
+    'breadcrumbs/title'       => 'Chemin',
+
+    /* BRAND PAGE*/
+    'unsorted'                => 'Non triés',
+    'engines'                 => 'Moteurs',
+    'concepts and prototypes' => 'Concepts et prototypes',
+    'logotypes'               => 'Logotypes',
+    'mixed'                   => 'Varié',
+
+    'count 0'                 => 'aucun',
+
+    /* CAR LIST */
+    'carlist/all pictures'    => 'toutes les images',
+    'carlist/details'         => 'détails',
+    'carlist/twins'           => 'des jumeaux',
+    'carlist/produced/one'    => 'Produit en un seul exemplaire',
+    'carlist/produced/%1$s'   => array(
+        'Produit en %1$s exemplaire',
+        'Produit en %1$s exemplaires'
+    ),
+    'carlist/produced-about/%1$s' => array(
+        'Produit en environ %1$s copie',
+        'Produit en environ %1$s copies'
+    ),
+    'carlist/specifications'  => 'Caractéristiques',
+    'carlist/designed by %s'  => 'dessinée par %s',
+
+    'present-time-abbr'       => 'pr.',
+
+    /* Categories */
+    'category/%s cars'        => array(
+        '%s voiture',
+        '%s voitures'
+    ),
+    'category/%s new cars'    => array(
+        '%s nouvelle',
+        '%s nouvelle'
+    ),
+
+    /* most */
+    'most/fastest' => 'le plus rapide',
+    'most/fastest/param' => 'vitesse maximal',
+    'most/slowest' => 'le plus lent',
+    'most/slowest/param' => 'vitesse maximal',
+    'most/dynamic' => 'dynamique',
+    'most/dynamic/param' => 'accélération',
+    'most/static' => 'statique',
+    'most/static/param' => 'accélération',
+    'most/mighty' => 'puissant',
+    'most/mighty/param' => 'la puissance du moteur',
+    'most/weak' => 'faible',
+    'most/weak/param' => 'la puissance du moteur',
+    'most/big-engine' => 'gros moteur',
+    'most/big-engine/param' => 'volume de moteur',
+    'most/small-engine' => 'petit moteur',
+    'most/small-engine/param' => 'volume de moteur',
+    'most/nimblest' => 'agile',
+    'most/nimblest/param' => 'braquage',
+    'most/economical' => 'économique',
+    'most/economical/param' => 'la consommation de carburant dans le cycle combiné',
+    'most/gluttonous' => 'glouton',
+    'most/gluttonous/param' => 'la consommation de carburant dans le cycle combiné',
+    'most/clenaly' => 'clenaly écologique',
+    'most/clenaly/param' => 'émission',
+    'most/dirty' => 'sale écologique',
+    'most/dirty/param' => 'émission',
+    'most/heavy' => 'lourd',
+    'most/heavy/param' => 'poids équipée',
+    'most/lightest' => 'plus léger',
+    'most/lightest/param' => 'poids équipée',
+    'most/longest' => 'la plus longue',
+    'most/longest/param' => 'longueur',
+    'most/shortest' => 'le plus court',
+    'most/shortest/param' => 'longueur',
+    'most/widest' => 'plus large',
+    'most/widest/param' => 'largeur',
+    'most/narrow' => 'étroit',
+    'most/narrow/param' => 'largeur',
+    'most/highest' => 'le plus haut',
+    'most/highest/param' => 'hauteur',
+    'most/lowest' => 'le plus bas',
+    'most/lowest/param' => 'hauteur',
+    'most/air' => 'air',
+    'most/air/param' => 'coefficients de traînée',
+    'most/antiair' => 'anti air',
+    'most/antiair/param' => 'coefficients de traînée',
+    'most/bigwheel' => 'grande roue',
+    'most/bigwheel/param' => 'taille de la roue',
+    'most/smallwheel' => 'petite roue',
+    'most/smallwheel/param' => 'taille de la roue',
+    'most/bigbrakes' => 'gros freins',
+    'most/bigbrakes/param' => 'freins taille',
+    'most/smallbrakes' => 'petits freins',
+    'most/smallbrakes/param' => 'freins taille',
+    'most/bigclearance' => 'grande garde au sol',
+    'most/bigclearance/param' => 'garde au sol',
+    'most/smallclearance' => 'petite garde au sol',
+    'most/smallclearance/param' => 'garde au sol',
+
+    /* upload */
+    'upload/picture/crop' => 'recadrer',
+    'upload/picture/cropped-to' => 'recadrée à %s',
+);
