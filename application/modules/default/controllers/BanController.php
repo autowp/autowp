a70ded35aaeb6063b4e57e89c557f87069215aa2
Dmitry <dmitry@pereslegin.ru>
2016-01-11 02:00:59 +0100

    ban & traffic controllers

---
+++ application/modules/default/controllers/BanController.php
@@ -0,0 +1,101 @@
+<?php
+
+class BanController extends Zend_Controller_Action
+{
+    public function unbanIpAction()
+    {
+        $canBan = $this->_helper->user()->isAllowed('user', 'ban');
+
+        $ip = $this->_getParam('ip');
+
+        if (!$canBan || $ip === null) {
+            return $this->_forward('notfound', 'error');
+        }
+
+        $service = new Application_Service_TrafficControl();
+        $service->unban($ip);
+
+        return $this->_redirect($this->getRequest()->getServer('HTTP_REFERER'));
+    }
+
+    public function banIpAction()
+    {
+        $canBan = $this->_helper->user()->isAllowed('user', 'ban');
+
+        $ip = $this->_getParam('ip');
+
+        if (!$canBan || $ip === null) {
+            return $this->_forward('notfound', 'error');
+        }
+
+        $service = new Application_Service_TrafficControl();
+
+        $service->ban(
+            $ip,
+            $this->getParam('period') * 3600,
+            $this->_helper->user()->get()->id,
+            $this->getParam('reason')
+        );
+
+        return $this->_redirect($this->getRequest()->getServer('HTTP_REFERER'));
+    }
+
+    public function banUserAction()
+    {
+        $users = new Users();
+        $user = $users->find($this->_getParam('user_id'))->current();
+
+        if (!$user) {
+            return $this->_forward('notfound', 'error');
+        }
+
+        $canBan = $this->_helper->user()->isAllowed('user', 'ban')
+              && ($this->_helper->user()->get()->id != $user->id);
+
+        if (!$canBan || $user->last_ip === null) {
+            return $this->_forward('notfound', 'error');
+        }
+
+        $service = new Application_Service_TrafficControl();
+
+        $service->ban(
+            inet_ntop($user->last_ip),
+            $this->getParam('period') * 3600,
+            $this->_helper->user()->get()->id,
+            $this->getParam('reason')
+        );
+
+        return $this->_redirect($this->_helper->url->url(array(
+            'action'   => 'user',
+            'user_id'  => $user->id,
+            'identity' => $user->identity
+        ), 'users', true));
+    }
+
+    public function unbanUserAction()
+    {
+        $users = new Users();
+        $user = $users->find($this->_getParam('user_id'))->current();
+
+        if (!$user) {
+            return $this->_forward('notfound', 'error');
+        }
+
+
+        $canBan = $this->_helper->user()->isAllowed('user', 'ban')
+              && ($this->_helper->user()->get()->id != $user->id);
+
+        if (!$canBan || $user->last_ip === null) {
+            return $this->_forward('notfound', 'error');
+        }
+
+        $service = new Application_Service_TrafficControl();
+        $service->unban(inet_ntop($user->last_ip));
+
+        return $this->_redirect($this->_helper->url->url(array(
+            'action'   => 'user',
+            'user_id'  => $user->id,
+            'identity' => $user->identity
+        ), 'users', true));
+    }
+}
