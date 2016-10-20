250dbdda580554c590d2db5e109a7746d2201cd2
Dmitry <dmitry@pereslegin.ru>
2016-10-20 19:57:58 +0300

    Favicons with bg

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,40 @@ gulp.task("copy.flags", function () {
         .pipe(gulpCopy("./public_html/img/vendor/flag-icon-css", {prefix: 4}));
 });
 
-gulp.task("favicon", function () {
+gulp.task("favicon-1", function () {
+    return gulp.src("./module/Application/assets/logo-bg.svg").pipe(favicons({
+        appName: "WheelsAge.org",
+        appDescription: "WheelsAge.org - encyclopedia of cars in the pictures",
+        developerName: "Dmitry P.",
+        developerURL: "https://github.com/autowp/autowp",
+        background: "#ede9de",
+        path: "/",
+        url: "https://wheelsage.org/",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        version: 1.0,
+        logging: false,
+        online: false,
+        html: "favicons.html",
+        pipeHTML: true,
+        replace: true,
+        icons: {
+            android: false,             // Create Android homescreen icon. `boolean`
+            appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset: offsetInPercentage }`
+            appleStartup: false,        // Create Apple startup images. `boolean`
+            coast: false,               // Create Opera Coast icon with offset 25%. `boolean` or `{ offset: offsetInPercentage }`
+            favicons: true,             // Create regular favicons. `boolean`
+            firefox: false,             // Create Firefox OS icons. `boolean` or `{ offset: offsetInPercentage }`
+            windows: false,             // Create Windows 8 tile icons. `boolean`
+            yandex: false               // Create Yandex browser icon. `boolean`
+        }
+    }))
+        .on("error", gutil.log)
+        .pipe(gulp.dest("./public_html"));
+});
+
+gulp.task("favicon-2", function () {
     return gulp.src("./module/Application/assets/logo.svg").pipe(favicons({
         appName: "WheelsAge.org",
         appDescription: "WheelsAge.org - encyclopedia of cars in the pictures",
@@ -113,10 +146,24 @@ gulp.task("favicon", function () {
         online: false,
         html: "favicons.html",
         pipeHTML: true,
-        replace: true
+        replace: true,
+        icons: {
+            android: true,               // Create Android homescreen icon. `boolean`
+            appleIcon: false,            // Create Apple touch icons. `boolean` or `{ offset: offsetInPercentage }`
+            appleStartup: true,          // Create Apple startup images. `boolean`
+            coast: { offset: 25 },       // Create Opera Coast icon with offset 25%. `boolean` or `{ offset: offsetInPercentage }`
+            favicons: false,             // Create regular favicons. `boolean`
+            firefox: true,               // Create Firefox OS icons. `boolean` or `{ offset: offsetInPercentage }`
+            windows: true,               // Create Windows 8 tile icons. `boolean`
+            yandex: true                 // Create Yandex browser icon. `boolean`
+        }
     }))
-    .on("error", gutil.log)
-    .pipe(gulp.dest("./public_html"));
+        .on("error", gutil.log)
+        .pipe(gulp.dest("./public_html"));
+});
+
+gulp.task('favicon', ['favicon-1', 'favicon-2'], function () {
+    return gulp;
 });
 
 /*gulp.task("minify.brands.png", function () {
