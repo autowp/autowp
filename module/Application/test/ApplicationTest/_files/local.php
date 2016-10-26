e2299b48c7f0aa31f53854c0dd519e615d59d7a2
Dmitry <dmitry@pereslegin.ru>
2016-10-26 16:10:05 +0300

    travis cache config

--- module/Application/test/ApplicationTest/_files/local.php
+++ module/Application/test/ApplicationTest/_files/local.php
@@ -19,4 +19,39 @@ return [
             'type' => 'in-memory'
         ],
     ],
+    'cachemanager' => [
+        'fast' => [
+            'frontend' => [
+                'name' => 'Core',
+                'customFrontendNaming' => 0,
+                'options' => [
+                    'lifetime' => 1800,
+                    'automatic_serialization' => true
+                ]
+            ],
+            'backend' => [
+                'name' => 'black-hole'
+            ]
+        ]
+    ],
+    'caches' => [
+        'fastCache' => [
+            'adapter' => [
+                'name'     =>'blackHole',
+                'lifetime' => 180,
+            ],
+        ],
+        'longCache' => [
+            'adapter' => [
+                'name'     =>'blackHole',
+                'lifetime' => 600
+            ],
+        ],
+        'localeCache' => [
+            'adapter' => [
+                'name'     =>'blackHole',
+                'lifetime' => 600
+            ],
+        ],
+    ],
 ];
