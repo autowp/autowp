80de95bbf8fe636189de36c0683c18820741b565
autow <autow@DESKTOP-H7022FJ>
2016-10-30 22:28:00 +0300

    Controllers tests initial

--- module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -17,13 +17,13 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
 
     public function testIndexActionCanBeAccessed()
     {
-        //$this->dispatch('https://www.autowp.ru/', 'GET');
+        $this->dispatch('https://www.autowp.ru/', 'GET');
 
-        /*$this->assertResponseStatusCode(200);
+        $this->assertResponseStatusCode(200);
         $this->assertModuleName('application');
         $this->assertControllerName(IndexController::class); // as specified in router's controller name alias
         $this->assertControllerClass('IndexController');
-        $this->assertMatchedRouteName('index');*/
+        $this->assertMatchedRouteName('index');
     }
 
     /*public function testIndexActionViewModelTemplateRenderedWithinLayout()
