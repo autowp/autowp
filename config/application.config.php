f837a94d674614312c42cb507744dd54ad6c68de
Dmitry <dmitry@pereslegin.ru>
2016-11-17 23:48:52 +0300

    Enable config cache

--- config/application.config.php
+++ config/application.config.php
@@ -49,7 +49,7 @@ return [
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        'config_cache_enabled' => false,
+        'config_cache_enabled' => true,
 
         // The key used to create the configuration cache file name.
         'config_cache_key' => 'config_cache',
@@ -57,7 +57,7 @@ return [
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        'module_map_cache_enabled' => false,
+        'module_map_cache_enabled' => true,
 
         // The key used to create the class map cache file name.
         'module_map_cache_key' => 'module_map_cache',
