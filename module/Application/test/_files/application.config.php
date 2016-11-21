fd7c175e31e3e0ddaecbb7b67524d3e3c947b7fb
autowp <dmitry@pereslegin.ru>
2016-11-21 22:22:25 +0300

    Move tests upper

--- module/Application/test/_files/application.config.php
+++ module/Application/test/_files/application.config.php
@@ -11,12 +11,12 @@ $configOverrides = [
 ];
 
 $merged = ArrayUtils::merge(
-    include __DIR__ . '/../../../../../config/application.config.php',
+    include __DIR__ . '/../../../../config/application.config.php',
     $configOverrides
 );
 
 $merged['module_listener_options']['config_glob_paths'] = [
-    'module/Application/test/ApplicationTest/_files/local.php',
+    'module/Application/test/_files/local.php',
 ];
 
 return $merged;
