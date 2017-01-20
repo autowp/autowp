c92874f8329c639654f37ffc3067bfc5ee15775d
autowp <dmitry@pereslegin.ru>
2017-01-21 02:50:17 +0300

    Cover Api/ContactsController

---
+++ module/Application/test/Controller/Api/ContactsControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ApplicationTest\Controller\Api;
+
+use Zend\Http\Header\Cookie;
+use Zend\Http\Request;
+use Zend\Json\Json;
+
+use Application\Controller\Api\ContactsController;
+use Application\Test\AbstractHttpControllerTestCase;
+
+class ContactsControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $applicationConfigPath = __DIR__ . '/../../_files/application.config.php';
+
+    public function testCreateDeleteContact()
+    {
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $this->dispatch('https://www.autowp.ru/api/contacts/1', Request::METHOD_PUT);
+
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(ContactsController::class);
+        $this->assertMatchedRouteName('api/contacts');
+        $this->assertActionName('update');
+
+        $json = Json::decode($this->getResponse()->getContent(), Json::TYPE_ARRAY);
+
+        $this->assertTrue($json['status']);
+
+        $this->reset();
+
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $this->dispatch('https://www.autowp.ru/api/contacts/1', Request::METHOD_DELETE);
+
+        $this->assertResponseStatusCode(204);
+        $this->assertModuleName('application');
+        $this->assertControllerName(ContactsController::class);
+        $this->assertMatchedRouteName('api/contacts');
+        $this->assertActionName('delete');
+    }
+
+}
