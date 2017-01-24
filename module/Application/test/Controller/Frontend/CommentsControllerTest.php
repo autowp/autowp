c4f45c3e7129407e52f0700d3537aef6fed91b4e
autowp <dmitry@pereslegin.ru>
2017-01-25 01:49:54 +0300

    Cover comments

--- module/Application/test/Controller/Frontend/CommentsControllerTest.php
+++ module/Application/test/Controller/Frontend/CommentsControllerTest.php
@@ -112,6 +112,19 @@ class CommentsControllerTest extends AbstractHttpControllerTestCase
         $json = Json::decode($this->getResponse()->getContent(), Json::TYPE_ARRAY);
 
         $this->assertTrue($json['ok']);
+
+        // get votes
+        $this->reset();
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=token'));
+        $this->dispatch('https://www.autowp.ru/comments/votes', Request::METHOD_GET, [
+            'id' => $comment['id']
+        ]);
+
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(CommentsController::class);
+        $this->assertMatchedRouteName('comments/votes');
+        $this->assertActionName('votes');
     }
 
     public function testCreateCommentAndDeleteAndRestore()
@@ -165,5 +178,42 @@ class CommentsControllerTest extends AbstractHttpControllerTestCase
         $json = Json::decode($this->getResponse()->getContent(), Json::TYPE_ARRAY);
 
         $this->assertTrue($json['ok']);
+    }
+
+    public function testCreateCommentAndResolve()
+    {
+        // create comment
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $this->dispatch('https://www.autowp.ru/comments/add/type_id/1/item_id/1', Request::METHOD_POST, [
+            'moderator_attention' => 0,
+            'parent_id'           => null,
+            'message'             => 'Test comment',
+            'moderator_attention' => 1
+        ]);
+
+        $this->assertResponseStatusCode(302);
+        $this->assertModuleName('application');
+        $this->assertControllerName(CommentsController::class);
+        $this->assertMatchedRouteName('comments/add');
+        $this->assertActionName('add');
+
+        $comment = $this->fetchLastComment();
+
+        // resolve
+        $this->reset();
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $url = 'https://www.autowp.ru/comments/add/type_id/'.$comment['type_id'].'/item_id/' . $comment['item_id'];
+        $this->dispatch($url, Request::METHOD_POST, [
+            'moderator_attention' => 0,
+            'parent_id'           => $comment['id'],
+            'message'             => 'Resolve',
+            'resolve'             => 1
+        ]);
+
+        $this->assertResponseStatusCode(302);
+        $this->assertModuleName('application');
+        $this->assertControllerName(CommentsController::class);
+        $this->assertMatchedRouteName('comments/add');
+        $this->assertActionName('add');
     }
 }
