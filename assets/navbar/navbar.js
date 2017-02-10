b529c2a91a8dc871d65122e3282b559b2a65ade9
autowp <dmitry@pereslegin.ru>
2017-02-10 22:28:57 +0000

    i18n

--- assets/navbar/navbar.js
+++ assets/navbar/navbar.js
@@ -1,6 +1,7 @@
 require("./navbar.less");
 
 var $ = require("jquery");
+var i18next = require('i18next');
 
 module.exports = {};
 
@@ -16,6 +17,11 @@ module.exports = {
             function reload() {
                 if (!$modal) {
                     $modal = $(require('./online.html'));
+                    
+                    $modal.find('.modal-title').text(i18next.t("who-online/title"));
+                    $modal.find('.btn-primary').text(i18next.t("who-online/refresh"));
+                    $modal.find('.btn-default').text(i18next.t("who-online/close"));
+                    
                     $body = $modal.find('.modal-body');
                     $btnRefresh = $modal.find('.btn-primary').on('click', function(e) {
                         e.preventDefault();
