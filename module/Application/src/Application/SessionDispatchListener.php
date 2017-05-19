f8d7b88edc2a38b20c6d8e61f6cb42fbc7e61a82
Dmitry <dmitry@pereslegin.ru>
2017-05-19 15:04:52 +0300

    Delete obsolete session cookie

--- module/Application/src/Application/SessionDispatchListener.php
+++ module/Application/src/Application/SessionDispatchListener.php
@@ -35,11 +35,17 @@ class SessionDispatchListener extends AbstractListenerAggregate
             $cookieDomain = $this->getHostCookieDomain($request, $config['hosts']);
             if ($cookieDomain) {
                 $sessionConfig->setCookieDomain($cookieDomain);
-                try {
-                    $sessionManager->start();
-                } catch (\Exception $e) {
-                    session_unset();
-                }
+            }
+            
+            try {
+                $sessionManager->start();
+            } catch (\Exception $e) {
+                session_unset();
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );
             }
 
             /*$cookies = $request->getCookie();
