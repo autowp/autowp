00b8dbb696d5f56b49bb208b4f040c234e550cd1
Dmitry <dmitry@pereslegin.ru>
2017-08-04 02:19:09 +0300

    Cover category index

--- module/Application/test/Controller/Frontend/CategoryControllerTest.php
+++ module/Application/test/Controller/Frontend/CategoryControllerTest.php
@@ -4,15 +4,128 @@ namespace ApplicationTest\Frontend\Controller;
 
 use Zend\Http\Header\Cookie;
 use Zend\Http\Request;
+use Zend\Json\Json;
 
+use Application\Controller\Api\ItemController;
+use Application\Controller\Api\ItemParentController;
+use Application\Controller\UploadController;
 use Application\Test\AbstractHttpControllerTestCase;
 
 class CategoryControllerTest extends AbstractHttpControllerTestCase
 {
     protected $applicationConfigPath = __DIR__ . '/../../../../../config/application.config.php';
 
+    private function createItem($params)
+    {
+        $this->reset();
+
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $this->dispatch('https://www.autowp.ru/api/item', Request::METHOD_POST, $params);
+
+        $this->assertResponseStatusCode(201);
+        $this->assertModuleName('application');
+        $this->assertControllerName(ItemController::class);
+        $this->assertMatchedRouteName('api/item/post');
+        $this->assertActionName('post');
+
+        $headers = $this->getResponse()->getHeaders();
+        $uri = $headers->get('Location')->uri();
+        $parts = explode('/', $uri->getPath());
+        $itemId = $parts[count($parts) - 1];
+
+        return $itemId;
+    }
+
+    private function addItemParent($itemId, $parentId, array $params = [])
+    {
+        $this->reset();
+
+        $this->getRequest()->getHeaders()->addHeader(Cookie::fromString('Cookie: remember=admin-token'));
+        $this->dispatch(
+            'https://www.autowp.ru/api/item-parent',
+            Request::METHOD_POST,
+            array_replace([
+                'item_id'   => $itemId,
+                'parent_id' => $parentId
+            ], $params)
+        );
+
+        $this->assertResponseStatusCode(201);
+        $this->assertModuleName('application');
+        $this->assertControllerName(ItemParentController::class);
+        $this->assertMatchedRouteName('api/item-parent/post');
+        $this->assertActionName('post');
+    }
+
+    private function addPictureToItem($vehicleId)
+    {
+        $this->reset();
+
+        $request = $this->getRequest();
+        $request->getHeaders()
+        ->addHeader(Cookie::fromString('Cookie: remember=admin-token'))
+        ->addHeaderLine('Content-Type', 'multipart/form-data');
+        $request->getServer()->set('REMOTE_ADDR', '127.0.0.1');
+
+        $file = tempnam(sys_get_temp_dir(), 'upl');
+        $filename = 'test.jpg';
+        copy(__DIR__ . '/../../_files/' . $filename, $file);
+
+        $request->getFiles()->fromArray([
+            'picture' => [
+                [
+                    'tmp_name' => $file,
+                    'name'     => $filename,
+                    'error'    => UPLOAD_ERR_OK,
+                    'type'     => 'image/jpeg'
+                ]
+            ]
+        ]);
+        $url = 'https://www.autowp.ru/upload/send/type/1/item_id/' . $vehicleId;
+        $this->dispatch($url, Request::METHOD_POST, [], true);
+
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(UploadController::class);
+        $this->assertMatchedRouteName('upload/params');
+        $this->assertActionName('send');
+
+        $this->assertResponseHeaderContains('Content-Type', 'application/json; charset=utf-8');
+
+        $json = Json::decode($this->getResponse()->getContent(), Json::TYPE_ARRAY);
+
+        $this->assertInternalType('array', $json);
+        $this->assertNotEmpty($json);
+
+        foreach ($json as $item) {
+            $this->assertArrayHasKey('id', $item);
+            $this->assertArrayHasKey('html', $item);
+            $this->assertArrayHasKey('width', $item);
+            $this->assertArrayHasKey('height', $item);
+        }
+
+        return $json[0]['id'];
+    }
+
     public function testIndex()
     {
+        $name = 'Category ' . microtime(true);
+
+        $categoryId = $this->createItem([
+            'item_type_id' => 3,
+            'name'         => $name
+        ]);
+
+        $vehicleId = $this->createItem([
+            'item_type_id' => 1,
+            'name'         => 'Vehicle in category'
+        ]);
+
+        $this->addItemParent($vehicleId, $categoryId);
+
+        $this->addPictureToItem($vehicleId);
+
+        $this->reset();
         $this->dispatch('https://www.autowp.ru/category', Request::METHOD_GET);
 
         $this->assertResponseStatusCode(200);
