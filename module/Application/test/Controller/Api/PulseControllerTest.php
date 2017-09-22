b34000d3ed6b390d8b0e425bed93e0dc0c49b09b
Dmitry <dmitry@pereslegin.ru>
2017-09-22 17:35:29 +0300

    Pulse test

--- module/Application/test/Controller/Api/PulseControllerTest.php
+++ module/Application/test/Controller/Api/PulseControllerTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace ApplicationTest\Frontend\Controller;
+namespace ApplicationTest\Controller\Api;
 
 use Application\Test\AbstractHttpControllerTestCase;
 
-use Application\Controller\PulseController;
+use Application\Controller\Api\PulseController;
 
 class PulseControllerTest extends AbstractHttpControllerTestCase
 {
@@ -12,14 +12,12 @@ class PulseControllerTest extends AbstractHttpControllerTestCase
 
     public function testIndex()
     {
-        $this->dispatch('https://www.autowp.ru/pulse', 'GET');
+        $this->dispatch('https://www.autowp.ru/api/pulse', 'GET');
 
         $this->assertResponseStatusCode(200);
         $this->assertModuleName('application');
         $this->assertControllerName(PulseController::class);
-        $this->assertMatchedRouteName('pulse');
+        $this->assertMatchedRouteName('api/pulse');
         $this->assertActionName('index');
-
-        $this->assertQuery("h1");
     }
 }
