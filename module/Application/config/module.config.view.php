6114e1acd8c5e7ad5488feb0aa1780ce71f2f265
Dmitry <dmitry@pereslegin.ru>
2017-10-16 03:13:51 +0300

    Remove flash messenger plugin

--- module/Application/config/module.config.view.php
+++ module/Application/config/module.config.view.php
@@ -54,11 +54,5 @@ return [
             'userText'       => View\Helper\Service\UserTextFactory::class,
         ]
     ],
-    'view_helper_config' => [
-        'flashmessenger' => [
-            'message_open_format'      => '<div%s><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><ul><li>',
-            'message_close_string'     => '</li></ul></div>',
-            'message_separator_string' => '</li><li>'
-        ]
-    ],
+    'view_helper_config' => [],
 ];
