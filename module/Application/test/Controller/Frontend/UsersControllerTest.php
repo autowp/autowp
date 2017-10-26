4ef7ea0c65067762a319b4ef61f394469a3f0fdc
Dmitry <dmitry@pereslegin.ru>
2017-10-26 23:49:50 +0300

    Remove oboslete test

--- module/Application/test/Controller/Frontend/UsersControllerTest.php
+++ module/Application/test/Controller/Frontend/UsersControllerTest.php
@@ -11,16 +11,6 @@ class UsersControllerTest extends AbstractHttpControllerTestCase
 {
     protected $applicationConfigPath = __DIR__ . '/../../../../../config/application.config.php';
 
-    public function testIndexActionCanBeAccessed()
-    {
-        $this->dispatch('https://www.autowp.ru/users/user1/comments', 'GET');
-
-        $this->assertResponseStatusCode(200);
-        $this->assertModuleName('application');
-        $this->assertControllerName(UsersController::class);
-        $this->assertMatchedRouteName('users/user/comments');
-    }
-
     public function testPictures()
     {
         $this->dispatch('https://www.autowp.ru/users/admin/pictures', Request::METHOD_GET);
