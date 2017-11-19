9b18a5ddd1b8f051cac6100dd4eefc25aab71c9b
Dmitry P. <dmitry@pereslegin.ru>
2017-11-20 02:06:00 +0300

    [ts] account/access

--- assets/controllers/account/access/index.ts
+++ assets/controllers/account/access/index.ts
@@ -6,7 +6,78 @@ import { IAutowpControllerScope } from 'declarations.d.ts';
 const CONTROLLER_NAME = 'AccountAccessController';
 const STATE_NAME = 'account-access';
 
+export class AccountAccessController {
+    static $inject = ['$scope', '$http', '$state', '$translate'];
+    public invalidParams: any;
+    public form: any;
+  
+    constructor(
+        private $scope: IAutowpControllerScope,
+        private $http: ng.IHttpService,
+        private $state: any,
+        private $translate: ng.translate.ITranslateService
+    ) {
+        if (! this.$scope.user) {
+            this.$state.go('login');
+            return;
+        }
+        
+        this.form = {
+            password_old: null,
+            password: null,
+            password_confirm: null
+        };
+        
+        this.invalidParams = {};
+        
+        this.$scope.pageEnv({
+            layout: {
+                blankPage: false,
+                needRight: false
+            },
+            name: 'page/133/name',
+            pageId: 133
+        });
+    }
+    
+    public submit() {
+        this.invalidParams = {};
+        
+        var self = this;
+        
+        this.$http({
+            method: 'PUT',
+            url: '/api/user/me',
+            data: this.form
+        }).then(function() {
+            
+            self.form = {
+                password_old: null,
+                password: null,
+                password_confirm: null
+            };
+            
+            self.$translate('account/access/change-password/saved').then(function(translation) {
+                notify({
+                    icon: 'fa fa-check',
+                    message: translation
+                }, {
+                    type: 'success'
+                });
+            });
+            
+        }, function(response) {
+            if (response.status == 400) {
+                self.invalidParams = response.data.invalid_params;
+            } else {
+                notify.response(response);
+            }
+        });
+    };
+};
+
 angular.module(Module)
+    .controller(CONTROLLER_NAME, AccountAccessController)
     .config(['$stateProvider',
         function config($stateProvider: any) {
             $stateProvider.state( {
@@ -17,68 +88,5 @@ angular.module(Module)
                 template: require('./template.html')
             });
         }
-    ])
-    .controller(CONTROLLER_NAME, [
-        '$scope', '$http', '$state', '$translate',
-        function($scope: IAutowpControllerScope, $http: ng.IHttpService, $state: any, $translate: ng.translate.ITranslateService) {
-            
-            if (! $scope.user) {
-                $state.go('login');
-                return;
-            }
-            
-            var ctrl = this;
-            
-            ctrl.form = {
-                password_old: null,
-                password: null,
-                password_confirm: null
-            };
-            
-            ctrl.invalidParams = {};
-            
-            $scope.pageEnv({
-                layout: {
-                    blankPage: false,
-                    needRight: false
-                },
-                name: 'page/133/name',
-                pageId: 133
-            });
-            
-            ctrl.submit = function() {
-                ctrl.invalidParams = {};
-                
-                $http({
-                    method: 'PUT',
-                    url: '/api/user/me',
-                    data: ctrl.form
-                }).then(function() {
-                    
-                    ctrl.form = {
-                        password_old: null,
-                        password: null,
-                        password_confirm: null
-                    };
-                    
-                    $translate('account/access/change-password/saved').then(function(translation) {
-                        notify({
-                            icon: 'fa fa-check',
-                            message: translation
-                        }, {
-                            type: 'success'
-                        });
-                    });
-                    
-                }, function(response) {
-                    if (response.status == 400) {
-                        ctrl.invalidParams = response.data.invalid_params;
-                    } else {
-                        notify.response(response);
-                    }
-                });
-            };
-        }
     ]);
 
-export default CONTROLLER_NAME;
