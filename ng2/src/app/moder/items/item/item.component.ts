fc2e3e6cca06e193c69f95f08d744d30b750b76f
Dmitry P <dmitry@pereslegin.ru>
2018-06-12 03:02:54 +0300

    moder/items/item

--- ng2/src/app/moder/items/item/item.component.ts
+++ ng2/src/app/moder/items/item/item.component.ts
@@ -10,10 +10,20 @@ import { ACLService } from '../../../services/acl.service';
 import { ItemService, APIItem } from '../../../services/item';
 import { chunkBy } from '../../../chunk';
 import { TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { Subscription, of, combineLatest } from 'rxjs';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { APIPicture, PictureService } from '../../../services/picture';
 import { PageEnvService } from '../../../services/page-env.service';
+import {
+  distinctUntilChanged,
+  debounceTime,
+  switchMap,
+  finalize,
+  catchError,
+  tap,
+  switchMapTo
+} from 'rxjs/operators';
+import Notify from '../../../notify';
 
 // Acl.isAllowed('car', 'edit_meta', 'unauthorized');
 
@@ -40,7 +50,6 @@ interface Tab {
 })
 @Injectable()
 export class ModerItemsItemComponent implements OnInit, OnDestroy {
-  private querySub: Subscription;
   private routeSub: Subscription;
   public loading = 0;
 
@@ -106,127 +115,135 @@ export class ModerItemsItemComponent implements OnInit, OnDestroy {
         () => (this.canEditSpecifications = false)
       );
 
-    this.querySub = this.route.queryParams.subscribe(params => {
-      this.activeTab = params.tab ? params.tab : 'meta';
+    this.routeSub = this.route.params
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(30),
+        switchMap(params => {
+          this.loading++;
+          return this.itemService.getItem(params.id, {
+            fields: [
+              'name_text',
+              'name_html',
+              'name',
+              'is_concept',
+              'name_default',
+              'body',
+              'subscription',
+              'begin_year',
+              'begin_month',
+              'end_year',
+              'end_month',
+              'today',
+              'begin_model_year',
+              'end_model_year',
+              'produced',
+              'is_group',
+              'spec_id',
+              'childs_count',
+              'full_name',
+              'catname',
+              'lat',
+              'lng',
+              'pictures_count',
+              'specifications_count',
+              'links_count',
+              'parents_count',
+              'item_language_count',
+              'engine_vehicles_count',
+              'logo'
+            ].join(',')
+          });
+        }),
+        finalize(() => {
+          this.loading--;
+        }),
+        catchError((err, caught) => {
+          Notify.response(err);
+          this.router.navigate(['/error-404']);
+          return of(null);
+        }),
+        tap(item => {
+          this.item = item;
 
-      switch (this.activeTab) {
-        case 'tree':
-          if (!this.treeTab.initialized) {
-            this.treeTab.initialized = true;
-            this.initTreeTab();
-          }
-          break;
-      }
-    });
+          const typeID = item.item_type_id;
 
-    this.routeSub = this.route.params.subscribe(params => {
-      this.loading++;
-      this.itemService
-        .getItem(params.id, {
-          fields: [
-            'name_text',
-            'name_html',
-            'name',
-            'is_concept',
-            'name_default',
-            'body',
-            'subscription',
-            'begin_year',
-            'begin_month',
-            'end_year',
-            'end_month',
-            'today',
-            'begin_model_year',
-            'end_model_year',
-            'produced',
-            'is_group',
-            'spec_id',
-            'childs_count',
-            'full_name',
-            'catname',
-            'lat',
-            'lng',
-            'pictures_count',
-            'specifications_count',
-            'links_count',
-            'parents_count',
-            'item_language_count',
-            'engine_vehicles_count',
-            'logo'
-          ].join(',')
-        })
-        .subscribe(
+          this.specsAllowed = [1, 2].indexOf(typeID) !== -1;
+
+          this.nameTab.count = item.item_language_count;
+          this.logoTab.count = item.logo ? 1 : 0;
+          this.catalogueTab.count = item.parents_count + item.childs_count;
+          this.vehiclesTab.count = item.engine_vehicles_count;
+          this.picturesTab.count = item.pictures_count;
+          this.linksTab.count = item.links_count;
+
+          this.metaTab.visible = true;
+          this.nameTab.visible = true;
+          this.catalogueTab.visible = typeID !== 7;
+          this.treeTab.visible = typeID !== 7;
+          this.linksTab.visible = [5, 7, 8].indexOf(typeID) !== -1;
+          this.logoTab.visible = typeID === 5;
+          this.vehiclesTab.visible = typeID === 2;
+          this.picturesTab.visible = [2, 1, 5, 6, 7, 8].indexOf(typeID) !== -1;
+        }),
+        switchMap(
           item => {
-            this.item = item;
-
-            this.specsAllowed = [1, 2].indexOf(this.item.item_type_id) !== -1;
-
-            this.translate
-              .get('item/type/' + this.item.item_type_id + '/name')
-              .subscribe(translation => {
-                this.pageEnv.set({
-                  layout: {
-                    isAdminPage: true,
-                    needRight: false
-                  },
-                  name: 'page/78/name',
-                  pageId: 78,
-                  args: {
-                    CAR_ID: this.item.id + '',
-                    CAR_NAME: translation + ': ' + this.item.name_text
-                  }
-                });
-              });
-            this.nameTab.count = this.item.item_language_count;
-            this.logoTab.count = this.item.logo ? 1 : 0;
-            this.catalogueTab.count =
-              this.item.parents_count + this.item.childs_count;
-            this.vehiclesTab.count = this.item.engine_vehicles_count;
-            this.picturesTab.count = this.item.pictures_count;
-            this.linksTab.count = this.item.links_count;
-
-            this.metaTab.visible = true;
-            this.nameTab.visible = true;
-            this.catalogueTab.visible = this.item.item_type_id !== 7;
-            this.treeTab.visible = this.item.item_type_id !== 7;
-            this.linksTab.visible =
-              [5, 7, 8].indexOf(this.item.item_type_id) !== -1;
-            this.logoTab.visible = this.item.item_type_id === 5;
-            this.vehiclesTab.visible = this.item.item_type_id === 2;
-            this.picturesTab.visible =
-              [2, 1, 5, 6, 7, 8].indexOf(this.item.item_type_id) !== -1;
-
             this.loading++;
-            this.pictureService
-              .getPictures({
+            return combineLatest(
+              this.translate.get('item/type/' + item.item_type_id + '/name'),
+              this.pictureService.getPictures({
                 fields: 'thumb_medium',
                 limit: 1,
-                item_id: this.item.id
+                item_id: item.id
               })
-              .subscribe(
-                response => {
-                  this.randomPicture =
-                    response.pictures.length > 0 ? response.pictures[0] : null;
-                  this.loading--;
-                },
-                () => {
-                  this.loading--;
-                }
-              );
-
-            this.loading--;
+            );
           },
-          () => {
-            this.router.navigate(['/error-404']);
-            this.loading--;
+          (item, combined) => ({
+            item: item,
+            translation: combined[0],
+            pictures: combined[1].pictures
+          })
+        ),
+        finalize(() => {
+          this.loading--;
+        }),
+        tap(data => {
+          this.pageEnv.set({
+            layout: {
+              isAdminPage: true,
+              needRight: false
+            },
+            name: 'page/78/name',
+            pageId: 78,
+            args: {
+              CAR_ID: data.item.id + '',
+              CAR_NAME: data.translation + ': ' + data.item.name_text
+            }
+          });
+          this.randomPicture =
+            data.pictures.length > 0 ? data.pictures[0] : null;
+        }),
+        switchMapTo(this.route.queryParams),
+        distinctUntilChanged(),
+        debounceTime(30),
+        tap(params => {
+          this.activeTab = params.tab ? params.tab : 'meta';
+
+          switch (this.activeTab) {
+            case 'tree':
+              if (!this.treeTab.initialized) {
+                this.treeTab.initialized = true;
+                this.initTreeTab();
+              }
+              break;
           }
-        );
-    });
+        })
+      )
+      .subscribe();
   }
 
   ngOnDestroy(): void {
     this.routeSub.unsubscribe();
-    this.querySub.unsubscribe();
   }
 
   private initTreeTab() {
