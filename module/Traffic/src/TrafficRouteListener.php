57f7f3e1e441a96d050e21718e49e7f4c1a28cfb
Dmitry P <dmitry@pereslegin.ru>
2018-07-18 18:34:32 +0300

    Whitelist some urls

--- module/Traffic/src/TrafficRouteListener.php
+++ module/Traffic/src/TrafficRouteListener.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Autowp\Traffic;
 
 use Zend\Authentication\AuthenticationService;
@@ -11,6 +13,52 @@ use Autowp\User\Model\User;
 
 class TrafficRouteListener extends AbstractListenerAggregate
 {
+    private $whitelist = [
+        '/api/forum',
+        '/api/user',
+        '/api/account',
+        '/api/acl',
+        '/api/article',
+        '/api/attr',
+        '/api/chart',
+        '/api/comment',
+        '/api/contacts',
+        '/api/donate',
+        '/api/feedback',
+        '/api/hotlinks',
+        '/api/ip',
+        '/api/item-link',
+        '/api/language',
+        '/api/log',
+        '/api/login',
+        '/api/signin',
+        '/api/map',
+        '/api/message',
+        '/api/mosts',
+        '/api/page',
+        '/api/picture-moder-vote',
+        '/api/pulse',
+        '/api/rating',
+        '/api/recaptcha',
+        '/api/restore-password',
+        '/api/text',
+        '/api/timezone',
+        '/api/traffic',
+        '/api/spec',
+        '/api/stat',
+        '/api/vehicle-types',
+        '/api/perspective',
+        '/api/perspective-page',
+        '/api/picture-moder-vote-template',
+        '/api/voting',
+        '/ng/',
+        '/comments',
+        '/donate',
+        '/factory',
+        '/login',
+        '/telegram'
+    ];
+
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      *
@@ -22,6 +70,17 @@ class TrafficRouteListener extends AbstractListenerAggregate
         $this->listeners[] = $events->attach(MvcEvent::EVENT_ROUTE, [$this, 'onRoute'], -625);
     }
 
+    private function matchWhitelist(string $url): bool
+    {
+        foreach ($this->whitelist as $prefix) {
+            if (strncasecmp($prefix, $url, strlen($prefix)) === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * @param  MvcEvent $e
      * @return null
@@ -31,6 +90,10 @@ class TrafficRouteListener extends AbstractListenerAggregate
         $request = $e->getRequest();
 
         if ($request instanceof \Zend\Http\PhpEnvironment\Request) {
+            if ($this->matchWhitelist($request->getUri()->getPath())) {
+                return;
+            }
+
             $serviceManager = $e->getApplication()->getServiceManager();
 
             $auth = new AuthenticationService();
