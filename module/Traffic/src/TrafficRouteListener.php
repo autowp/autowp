b45fee5e72d2ba9ad4b58509d332e71f22cfe2c9
Dmitry P <dmitry@pereslegin.ru>
2018-10-07 22:37:19 +0300

    Check ip not empty

--- module/Traffic/src/TrafficRouteListener.php
+++ module/Traffic/src/TrafficRouteListener.php
@@ -111,19 +111,21 @@ class TrafficRouteListener extends AbstractListenerAggregate
 
             $ip = $request->getServer('REMOTE_ADDR');
 
-            $service = $serviceManager->get(TrafficControl::class);
+            if ($ip) {
+                $service = $serviceManager->get(TrafficControl::class);
 
-            $banInfo = $service->getBanInfo($ip);
-            if ($banInfo) {
-                $response = $e->getResponse();
-                $response->setStatusCode(403);
-                $response->setContent('Access denied: ' . $banInfo['reason']);
+                $banInfo = $service->getBanInfo($ip);
+                if ($banInfo) {
+                    $response = $e->getResponse();
+                    $response->setStatusCode(403);
+                    $response->setContent('Access denied: ' . $banInfo['reason']);
 
-                return $response;
-            }
+                    return $response;
+                }
 
-            if (! $unlimitedTraffic) { //  && ! $service->inWhiteList($ip)
-                $service->pushHit($ip);
+                if (! $unlimitedTraffic) { //  && ! $service->inWhiteList($ip)
+                    $service->pushHit($ip);
+                }
             }
         }
     }
