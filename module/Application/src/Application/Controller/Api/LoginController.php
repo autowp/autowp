df5f3fa7e0d47fa67a29a2d386397053c227feb8
Dmitry <dmitry@pereslegin.ru>
2019-04-26 00:07:14 +0300

    https in redirect

--- module/Application/src/Application/Controller/Api/LoginController.php
+++ module/Application/src/Application/Controller/Api/LoginController.php
@@ -265,7 +265,7 @@ class LoginController extends AbstractRestfulController
             $url = $this->url()->fromRoute('login/callback', [], [
                 'force_canonical' => true,
                 'query'           => $params,
-                'uri'             => new HttpUri('http://' . $this->hosts[$stateRow['language']]['hostname'])
+                'uri'             => new HttpUri('https://' . $this->hosts[$stateRow['language']]['hostname'])
             ]);
             return $this->redirect()->toUrl($url);
         }
