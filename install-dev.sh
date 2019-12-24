b1a9f486fe8c3b47ee538c7aa0a46fd1f2f4cd06
Dmitry P <dmitry@pereslegin.ru>
2019-12-25 01:29:13 +0300

    New CodeClimate reporter

--- install-dev.sh
+++ install-dev.sh
@@ -4,3 +4,6 @@ set -e
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y -qq php-ast php-xdebug mysql-client
 composer install --no-progress --no-interaction --no-suggest --optimize-autoloader
+
+curl -Ls https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
