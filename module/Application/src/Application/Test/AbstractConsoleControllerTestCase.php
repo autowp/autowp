01030a7e69d5745b2ff37fab167048168988e77c
Dmitry P <dmitry@pereslegin.ru>
2019-12-24 21:45:48 +0300

    PHPUnit 8

--- module/Application/src/Application/Test/AbstractConsoleControllerTestCase.php
+++ module/Application/src/Application/Test/AbstractConsoleControllerTestCase.php
@@ -10,7 +10,7 @@ abstract class AbstractConsoleControllerTestCase extends ZendTestCase
 {
     protected $applicationConfigPath;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         if (! $this->applicationConfigPath) {
             throw new Exception("Application config path not provided");
@@ -21,7 +21,7 @@ abstract class AbstractConsoleControllerTestCase extends ZendTestCase
         parent::setUp();
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         parent::tearDown();
 
