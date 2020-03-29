e5c01a1f2994c325c7848ae2e2f8af72c3077d36
Dmitry P <dmitry@pereslegin.ru>
2020-03-30 01:23:56 +0300

    Test token

--- module/Application/test/Data.php
+++ module/Application/test/Data.php
@@ -7,8 +7,8 @@ use Laminas\Http\Header\Authorization;
 class Data
 {
     public const ADMIN_AUTH_HEADER = 'Authorization: Bearer '
-        . 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZWZhdWx0IiwiZXhwIjoxNTg1NTA4MzUzLCJzdWIiOiIxIn0.'
-        . 'Ux76cJNicZLB15-t6TLmMGg2NtjM5zUA4BWkaqKk0dvB5TihzOtKkAH1cM_D18RIOZj6jDHH98sFMdbYFFdzRg';
+        . 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZWZhdWx0IiwiZXhwIjoxODgwMDAwMDAwLCJzdWIiOiIxIn0.'
+        . 'TJUsXWgvn1uEay6OIdFUHYoMsZuOjNOKKl-cQrMWsEcTTa-4_-Cob-w97VTWJOx0QJlGNhofw1vo2UUi-xxF-g';
 
     public static function getAdminAuthHeader(): Authorization
     {
