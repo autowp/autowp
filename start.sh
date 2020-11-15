bf98e5806d0185e0b04c9d9b33c3c145b52c4659
Dmitry P <dmitry@pereslegin.ru>
2020-11-15 16:47:30 +0300

    telegram location

--- start.sh
+++ start.sh
@@ -9,8 +9,6 @@ mkdir -p /app/public_html/img
 mkdir --mode=0777 -p /app/cache
 mkdir --mode=0777 -p /app/cache/modulecache
 
-ls -lah /app/cache/modulecache
-
 echo "Waiting for mysql"
 
 waitforit -host=mysql -port=3306 -timeout=60
