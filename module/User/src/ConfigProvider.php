c167c16d16114efee1252cf78cc2d53bfaa1fa49
Dmitry P <dmitry@pereslegin.ru>
2022-01-17 23:46:04 +0300

    Remove unused UserPasswordRemind code

--- module/User/src/ConfigProvider.php
+++ module/User/src/ConfigProvider.php
@@ -35,10 +35,9 @@ class ConfigProvider
     {
         return [
             'factories' => [
-                Model\User::class               => Model\UserFactory::class,
-                Model\UserPasswordRemind::class => Model\UserPasswordRemindFactory::class,
-                Model\UserRename::class         => Model\UserRenameFactory::class,
-                Service\OAuth::class            => Service\OAuthFactory::class,
+                Model\User::class       => Model\UserFactory::class,
+                Model\UserRename::class => Model\UserRenameFactory::class,
+                Service\OAuth::class    => Service\OAuthFactory::class,
             ],
         ];
     }
