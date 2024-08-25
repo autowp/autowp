5a400cca61b5af41f481e1c211ef0ae0382d8c9a
Dmitry P. <dmitry@pereslegin.ru>
2024-08-25 17:42:13 +0300

    test: remove unused test code

--- module/Application/test/Controller/Api/ItemControllerTest.php
+++ module/Application/test/Controller/Api/ItemControllerTest.php
@@ -4,9 +4,6 @@ namespace ApplicationTest\Controller\Api;
 
 use Application\Controller\Api\ItemController;
 use Application\Controller\Api\ItemParentController;
-use Application\Controller\Api\PictureController;
-use Application\Controller\Api\PictureItemController;
-use Application\DuplicateFinder;
 use Application\Model\Item;
 use Application\Test\AbstractHttpControllerTestCase;
 use ApplicationTest\Data;
@@ -18,14 +15,9 @@ use Laminas\Json\Json;
 
 use function array_pop;
 use function array_replace;
-use function copy;
 use function count;
 use function explode;
 use function microtime;
-use function sys_get_temp_dir;
-use function tempnam;
-
-use const UPLOAD_ERR_OK;
 
 class ItemControllerTest extends AbstractHttpControllerTestCase
 {
@@ -66,14 +58,6 @@ class ItemControllerTest extends AbstractHttpControllerTestCase
             'item_type_id' => 1,
             'name'         => 'Some vehicle',
         ], $params));
-    }
-
-    private function createEngine(): int
-    {
-        return $this->createItem([
-            'item_type_id' => 2,
-            'name'         => 'Some engine',
-        ]);
     }
 
     /**
@@ -159,100 +143,6 @@ class ItemControllerTest extends AbstractHttpControllerTestCase
         $this->assertControllerName(ItemParentController::class);
         $this->assertMatchedRouteName('api/item-parent/post');
         $this->assertActionName('post');
-    }
-
-    private function mockDuplicateFinder(): void
-    {
-        $serviceManager = $this->getApplicationServiceLocator();
-
-        $tables = $serviceManager->get('TableManager');
-
-        $mock = $this->getMockBuilder(DuplicateFinder::class)
-            ->onlyMethods(['indexImage'])
-            ->setConstructorArgs([
-                $serviceManager->get('RabbitMQ'),
-                $tables->get('df_distance'),
-            ])
-            ->getMock();
-
-        $mock->method('indexImage');
-
-        $serviceManager->setService(DuplicateFinder::class, $mock);
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function addPictureToItem(int $vehicleId): int
-    {
-        $this->reset();
-
-        $this->mockDuplicateFinder();
-
-        /** @var Request $request */
-        $request = $this->getRequest();
-        $request->getHeaders()
-            ->addHeader(Data::getAdminAuthHeader(
-                $this->getApplicationServiceLocator()->get('Config')['keycloak']
-            ))
-            ->addHeaderLine('Content-Type', 'multipart/form-data');
-        $request->getServer()->set('REMOTE_ADDR', '127.0.0.1');
-
-        $file     = tempnam(sys_get_temp_dir(), 'upl');
-        $filename = 'test.jpg';
-        copy(__DIR__ . '/../../_files/' . $filename, $file);
-
-        $request->getFiles()->fromArray([
-            'file' => [
-                'tmp_name' => $file,
-                'name'     => $filename,
-                'error'    => UPLOAD_ERR_OK,
-                'type'     => 'image/jpeg',
-            ],
-        ]);
-
-        $this->dispatch('https://www.autowp.ru/api/picture', Request::METHOD_POST, [
-            'item_id' => $vehicleId,
-        ]);
-
-        $this->assertResponseStatusCode(201);
-        $this->assertModuleName('application');
-        $this->assertControllerName(PictureController::class);
-        $this->assertMatchedRouteName('api/picture/post');
-        $this->assertActionName('post');
-
-        /** @var Response $response */
-        $response = $this->getResponse();
-        /** @var Location $location */
-        $location = $response->getHeaders()->get('Location');
-        $uri      = $location->uri();
-        $parts    = explode('/', $uri->getPath());
-        return (int) $parts[count($parts) - 1];
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function acceptPicture(int $pictureId): void
-    {
-        $this->reset();
-
-        /** @var Request $request */
-        $request = $this->getRequest();
-        $request->getHeaders()->addHeader(Data::getAdminAuthHeader(
-            $this->getApplicationServiceLocator()->get('Config')['keycloak']
-        ));
-        $this->dispatch(
-            'https://www.autowp.ru/api/picture/' . $pictureId,
-            Request::METHOD_PUT,
-            ['status' => 'accepted']
-        );
-
-        $this->assertResponseStatusCode(200);
-        $this->assertModuleName('application');
-        $this->assertControllerName(PictureController::class);
-        $this->assertMatchedRouteName('api/picture/picture/update');
-        $this->assertActionName('update');
     }
 
     /**
