30bf8c2c9f2e26eb8c266823111ef2239af96514
Dmitry P. <dmitry@pereslegin.ru>
2025-12-21 23:06:42 +0300

    feat: more RenderMode.Server pages

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'articles/**',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'donate/success',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'policy',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: 'telegram',
     renderMode: RenderMode.Server,
   },
