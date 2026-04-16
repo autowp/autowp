38400114c4939e73c4886af6e0636dedeafb3ddc
Dmitry P. <dmitry@pereslegin.ru>
2026-04-17 01:25:27 +0300

    refactor: articles table via postgres

--- goautowp/migrations/70_htmls.up.sql
+++ goautowp/migrations/70_htmls.up.sql
@@ -2,6 +2,6 @@ alter table article add column html longtext not null;
 
 update article set html=(select html from htmls where id = article.html_id);
 
-alter table article drop column html_id;
+alter table article drop constraint articles_fk2, drop column html_id;
 
 drop table htmls;
