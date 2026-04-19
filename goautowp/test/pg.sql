6f16324594a79131608e04b152aaa522b0630427
Dmitry P. <dmitry@pereslegin.ru>
2026-04-20 01:04:07 +0300

    test: test dump for postgres

---
+++ goautowp/test/pg.sql
@@ -0,0 +1,3 @@
+insert into article (id, html, name, catname, last_editor_id, last_edit_date, add_date, author_id,
+    enabled, first_enabled_datetime, description, preview_width, preview_height, preview_filename, ratio)
+values (1, 'Test html', 'Test article', 'test-article', null, null, now(), null, true, now(), 'Test description', 100, 100, 'test.jpg', 0);
